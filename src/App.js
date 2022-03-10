8fa3c5f12863b0d483ff17d1d2fe755c3f20958a
christinetrac <christineetrac@gmail.com>
2022-03-10 00:55:21 -0500

    transitions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
 import Home from "./Containers/Home/Home";
 import Acnh from "./Containers/Projects/Acnh";
 import Friendle from "./Containers/Projects/Friendle";
@@ -10,17 +12,27 @@ import Dimension from "./Containers/Projects/Dimension";
 
 function App() {
   const isMobile = useMediaQuery({ maxWidth: 520 })
+  const location = useLocation();
 
   return (
-    <Routes>
-      <Route exact path='/' element={<Home isMobile={isMobile}/>} />
-      <Route exact path='/acnh' element={<Acnh isMobile={isMobile}/>} />
-      <Route exact path='/friendle' element={<Friendle isMobile={isMobile}/>} />
-      <Route exact path='/aftrmarkt' element={<Market isMobile={isMobile}/>} />
-      <Route exact path='/glean' element={<Glean isMobile={isMobile}/>} />
-      <Route exact path='/kashkitty' element={<Kitty isMobile={isMobile}/>} />
-      <Route exact path='/dimension' element={<Dimension isMobile={isMobile}/>} />
-    </Routes>
+      <TransitionGroup component={null}>
+          <ScrollToTop />
+          <CSSTransition
+              key={location.key}
+              timeout={500}
+              classNames="fade"
+          >
+              <Routes location={location}>
+                  <Route exact path='/' element={<Home isMobile={isMobile}/>} />
+                  <Route exact path='/acnh' element={<Acnh isMobile={isMobile}/>} />
+                  <Route exact path='/friendle' element={<Friendle isMobile={isMobile}/>} />
+                  <Route exact path='/aftrmarkt' element={<Market isMobile={isMobile}/>} />
+                  <Route exact path='/glean' element={<Glean isMobile={isMobile}/>} />
+                  <Route exact path='/kashkitty' element={<Kitty isMobile={isMobile}/>} />
+                  <Route exact path='/dimension' element={<Dimension isMobile={isMobile}/>} />
+              </Routes>
+          </CSSTransition>
+      </TransitionGroup>
   );
 }
 
